9726016c32d88ebdc52744fbd3ce06620f8e3b18
nishapusp <puspender.sharma@gmail.com>
2024-10-27 01:27:13 +0530

    Update main_final1.py

--- main_final1.py
+++ main_final1.py
@@ -171,7 +171,7 @@ def save_application_data():
 def main():
     initialize_session_state()
 
-    st.title("MSME Loan Application")
+    st.title("MSME Loan Application(Beta)")
 
     # Define sections
     sections = [
